4b76411dde3c63e3ce19f2cfbbaca440a21e860c
rodrigo.toledo <rodrigo.toledo@creditcorp.com.br>
2022-07-24 03:12:22 -0300

    acoes em tempo real - broadcast com suggestions e stories

--- app/models/following_user.rb
+++ app/models/following_user.rb
@@ -1,4 +1,9 @@
 class FollowingUser < ApplicationRecord
   belongs_to :user
   belongs_to :following_user, class_name: "User"
+
+  after_commit do
+    broadcast_update_to(:suggestions, target: "suggestions_user_#{user_id}", partial: "components/suggestion", locals: {current_user: user})
+    broadcast_update_to(:suggestions, target: "stories_user_#{user_id}", partial: "components/story", locals: {current_user: user})
+  end
 end
