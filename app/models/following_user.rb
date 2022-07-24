c93c9396ae781c6bc0d54787582e765d253efb57
rodrigo.toledo <rodrigo.toledo@creditcorp.com.br>
2022-07-24 11:28:22 -0300

    refatorando - mudanca de nomenclaturas de parties - acoes de links agora sao possiveis

--- app/models/following_user.rb
+++ app/models/following_user.rb
@@ -3,7 +3,7 @@ class FollowingUser < ApplicationRecord
   belongs_to :following_user, class_name: "User"
 
   after_commit do
-    broadcast_update_to(:suggestions, target: "suggestions_user_#{user_id}", partial: "components/suggestion", locals: {current_user: user})
-    broadcast_update_to(:suggestions, target: "stories_user_#{user_id}", partial: "components/story", locals: {current_user: user})
+    broadcast_update_to(:suggestions, target: "suggestions_user_#{user_id}", partial: "components/suggestions_for_current_user", locals: {current_user: user})
+    broadcast_update_to(:suggestions, target: "stories_user_#{user_id}", partial: "components/stories_for_current_user", locals: {current_user: user})
   end
 end
