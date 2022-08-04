902d78b9139ab95b3bab1b7aa706dbe1e0c4f6e3
Rodrigo Toledo <rodrigo.toledo@creditcorp.com.br>
2022-08-04 00:02:46 -0300

    update the header with sum of posts for the current user

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,6 @@ class Post < ApplicationRecord
     self.likes_count = self.likes_count+1
     update_column(:likes_count, self.likes_count)
     broadcast_update(target: "count_of_#{id}", html: self.likes_count)
+    broadcast_update_to(:suggestions, target: "total_of_likes_user_#{user_id}", html: user.posts_likes_count)
   end
 end
