eee03acee58fdc8d213038a522ad44325fed65db
RToledo DEV <rodrigo@rtoledo.inf.br>
2022-10-22 06:51:31 -0300

    remving schema.rb from git fix menu icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 company = Company.create(name: Faker::Company.name)
-user_main = User.create(company: company, name: 'Rodrigo Toledo', email: 'faker@test.com', address: Faker::Address.full_address, avatar: Faker::Avatar.image, phone: Faker::PhoneNumber.cell_phone_with_country_code, username: 'rtoledo_dev', website: Faker::Internet.url, password: 'aassdd', password_confirmation: 'aassdd')
+User.create!(company: company, name: 'Admin test', email: 'admin@test.com', address: Faker::Address.full_address, avatar: Faker::Avatar.image, phone: Faker::PhoneNumber.cell_phone_with_country_code, username: 'admin_test', website: Faker::Internet.url, password: 'aassdd123', password_confirmation: 'aassdd123')
+user_main = User.create!(company: company, name: 'Rodrigo Toledo', email: 'faker@test.com', address: Faker::Address.full_address, avatar: Faker::Avatar.image, phone: Faker::PhoneNumber.cell_phone_with_country_code, username: 'rtoledo_dev', website: Faker::Internet.url, password: 'aassdd123', password_confirmation: 'aassdd123')
 10.times.each do
-  Post.create(user: user_main, title: Faker::Lorem.question, image: Faker::Avatar.image, caption: 'Lets build this faker instagram')
+  Post.create!(user: user_main, title: Faker::Lorem.question, image: Faker::Avatar.image, caption: 'Lets build this faker instagram')
 end
 
 19.times.each do
-   user = User.create(company: company, name: Faker::Name.name_with_middle, email: Faker::Internet.email, address: Faker::Address.full_address, avatar: Faker::Avatar.image, phone: Faker::PhoneNumber.cell_phone_with_country_code, username: Faker::Internet.username, website: Faker::Internet.url, password: 'aassdd', password_confirmation: 'aassdd')
+   user = User.create!(company: company, name: Faker::Name.name_with_middle, email: Faker::Internet.email, address: Faker::Address.full_address, avatar: Faker::Avatar.image, phone: Faker::PhoneNumber.cell_phone_with_country_code, username: Faker::Internet.username, website: Faker::Internet.url, password: 'aassdd123', password_confirmation: 'aassdd123')
    10.times.each do
       Post.create(user: user, title: Faker::Lorem.question, image: Faker::Avatar.image, caption: 'Lets build this faker instagram')
    end
